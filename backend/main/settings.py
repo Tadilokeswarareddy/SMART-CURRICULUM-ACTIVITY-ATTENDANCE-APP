10a705021bfe65ba8a9be613ac55727bf5fa59cf
Lokesh Reddy <lokeshreddy@Lokeshs-MacBook-Air-2.local>
2026-04-16 06:17:49 +0530

    backend deployment2

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -55,10 +55,14 @@ INSTALLED_APPS = [
 
 CORS_ALLOW_ALL_ORIGINS = False
 
+frontend_url = os.environ.get("FRONTEND_URL")
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    os.environ.get("FRONTEND_URL"),  
 ]
+
+if frontend_url:
+    CORS_ALLOWED_ORIGINS.append(frontend_url)
 
 CORS_ALLOW_CREDENTIALS = True
 
